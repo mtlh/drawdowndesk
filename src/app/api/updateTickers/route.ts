50ce3de3927714008f1855d37325dd700bde87a2
Matthew Harvey <matthewtlharvey@gmail.com>
2026-01-03 02:54:31 +0000

    use quoteCombine for refreshing prices

--- src/app/api/updateTickers/route.ts
+++ src/app/api/updateTickers/route.ts
@@ -6,31 +6,44 @@ const convex = new ConvexHttpClient(
   process.env.NEXT_PUBLIC_CONVEX_URL!
 );
 
-const yahooFinance = new YahooFinance({ suppressNotices: ['yahooSurvey'] });
+const yahooFinance = new YahooFinance({
+  suppressNotices: ['yahooSurvey'],
+  queue: {
+    concurrency: 1,
+  },
+  quoteCombine: {
+    maxSymbolsPerRequest: 50,
+    debounceTime: 1000,
+  },
+});
+
+const fields = ["regularMarketPrice", "currency", "symbol"];
 
 export async function GET() {
-    
-    const holdings = await convex.query(api.portfolio.currentPriceUpdates.updateHoldingWithTicker.getAllHoldings, {});
-
-    await Promise.all( 
-        holdings.map(async (ticker) => { 
-            //console.log(ticker)
-            const q = await yahooFinance.quote(ticker); 
-            // console.log(q)
-
-            // If currency is GBp convert to GBP
-            if (q.currency === "GBp") {
-                q.regularMarketPrice = q.regularMarketPrice / 100;
-            }
-
-            if (q) {
-                await convex.mutation(api.portfolio.currentPriceUpdates.updateHoldingWithTicker.updateHoldingWithTicker, {
-                    symbol: q.symbol,
-                    currentPrice: q.regularMarketPrice,
-                });
-            }
-        })
+    const holdings = await convex.query(
+        api.portfolio.currentPriceUpdates.updateHoldingWithTicker.getAllHoldings,
+        {}
     );
+
+    const promises = holdings.map(symbol => 
+        yahooFinance.quoteCombine(symbol, { fields })
+    ); 
+
+    const results = await Promise.all(promises);
+
+    for (const q of results) {
+        if (q.currency === "GBp") {
+            q.regularMarketPrice = q.regularMarketPrice / 100;
+        }
+
+        await convex.mutation(
+            api.portfolio.currentPriceUpdates.updateHoldingWithTicker.updateHoldingWithTicker,
+            {
+            symbol: q.symbol,
+            currentPrice: q.regularMarketPrice,
+            }
+        );
+    }
 
     return new Response("Success");
 }
