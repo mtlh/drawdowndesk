fcb99f2cc84bb1ebc13718710efc66135aa77fa5
Matthew Harvey <matthewtlharvey@gmail.com>
2026-04-01 20:02:03 +0100

    debug: add console logs for navigation events

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,26 @@ export default function Page() {
   const router = useRouter()
   
   useEffect(() => {
+    console.log('[Page] Mounted, adding event listeners')
+    
     const handlePopState = () => {
+      console.log('[Page] PopState event fired - reloading')
+      window.location.reload()
+    }
+    
+    const handlePageshow = (event: PageTransitionEvent) => {
+      console.log('[Page] Pageshow event fired, persisted:', event.persisted)
       window.location.reload()
     }
     
     window.addEventListener('popstate', handlePopState)
-    return () => window.removeEventListener('popstate', handlePopState)
+    window.addEventListener('pageshow', handlePageshow)
+    
+    return () => {
+      console.log('[Page] Unmounting, removing event listeners')
+      window.removeEventListener('popstate', handlePopState)
+      window.removeEventListener('pageshow', handlePageshow)
+    }
   }, [router])
   
   return <Login />
